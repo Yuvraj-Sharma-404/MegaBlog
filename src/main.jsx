fad5183833c97ed2fd67d816c168f21807c25a68
Yuvraj <yuvrajsharma21314@gmail.com>
2026-06-18 05:43:19 +0530

    bug fixed

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import Home from "./pages/Home.jsx";
 import { AuthLayout, Login } from "./components/index.js";
 
 import AddPost from "./pages/AddPost";
-import Signup from "./pages/Signup";
+import Signup from "./pages/SignUp";
 import EditPost from "./pages/EditPost.jsx";
 
 import Post from "./pages/Post";
